d441a450f508f79a0d4a0abc2199bcc5a6ecc650
maestro3000 <35443754+maestro3000@users.noreply.github.com>
2019-10-31 00:38:25 -0700

    Data Management
    Transferred over the data management code to master

---
+++ app/datamanagement.py
@@ -0,0 +1,17 @@
+from app import db
+from app.models import User, Post
+
+def deleteAllUsers():
+    for u in User.query.all():
+        db.session.delete(u)
+    db.session.commit()
+
+def deleteAllPosts():
+    for p in Post.query.all():
+        db.session.delete(p)
+    db.session.commit()
+
+def deleteAllUserPosts(user):
+    for p in user.posts:
+        db.session.delete(p)
+    db.session.commit()
