bce757655faf171b41ce42051e32bb390b1819f6
maestro3000 <35443754+maestro3000@users.noreply.github.com>
2019-11-02 13:51:38 -0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import User
 
 class userLogin(FlaskForm):
