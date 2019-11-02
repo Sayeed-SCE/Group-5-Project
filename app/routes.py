e8c91432ec377e0f55c2d31c9a346d8f2b7cd723
Sayeed Nj <48534405+Sayeed-SCE@users.noreply.github.com>
2019-11-02 13:43:45 -0700

    Log in
    Added log into use cases

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,28 @@
 from app import app
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import userLogin, userRegistry
+from app.models import User
 
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'David'}
     return render_template('index.html', title = 'Home', user = user)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = userLogin()
     title = 'Log in'
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(form.susername.data, form.remember_me))
-        return redirect('/index')
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember = form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', form = form, title = title)
 
 @app.route('/logout')
